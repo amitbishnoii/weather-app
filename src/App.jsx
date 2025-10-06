35d7340ba187ca3abb04675ff9a1cdb9984c69c9
amitbishnoii <amitbishnoi29012@gmail.com>
2025-10-06 21:45:00 +0530

    integrate api and improve styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,14 @@ import "./App.css"
 
 function App() {
   const [city, setcity] = useState("")
+  const [weather, setweather] = useState(null)
   const api = "f7c36c96b51e4e50b81145652250610"
 
-  const handleClick = () => {
-
+  const handleClick = async () => {
+    let r = await fetch(`https://api.weatherapi.com/v1/current.json?key=${api}&q=${city}`);
+    let res = await r.json()
+    setweather(res)
+    console.log(res);
   }
 
   return (
@@ -16,8 +20,13 @@ function App() {
       <Navbar />
       <div className="weather-card">
         <div className="city-bar">
-          <input className="input-city" type="text" placeholder="Enter City Name..." />
+          <input className="input-city" type="text" placeholder="Enter City Name..." value={city} onChange={(e) => { setcity(e.target.value) }} />
           <button onClick={handleClick}>Check</button>
+        </div>
+        <div className="weather-data">
+          {weather && <p>City: {weather.location.name}</p>}
+          {weather && <p>Region: {weather.location.region}</p>}
+          {weather && <p>Temprature: {weather.current.temp_c}</p>}
         </div>
       </div>
     </>
