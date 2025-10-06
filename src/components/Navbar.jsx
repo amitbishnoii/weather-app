2928f51971639688f161449f0fca7790ee1741bc
amitbishnoii <amitbishnoi29012@gmail.com>
2025-10-06 21:14:30 +0530

    add basic stylings

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div>
+        <ul>
+            <li>Github</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
