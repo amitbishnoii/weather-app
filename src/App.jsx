3310f842c3f08abfa387b99b581a1b691b0c200a
amitbishnoii <amitbishnoi29012@gmail.com>
2025-10-12 19:42:47 +0530

    update styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from "react"
 import Navbar from "./components/Navbar"
 import "./App.css"
 import { FaLocationArrow } from "react-icons/fa";
+import { BsThermometerHalf } from "react-icons/bs";
+import { WiBarometer } from "react-icons/wi";
+import { MdOutlineVisibility } from "react-icons/md";
 
 
 function App() {
@@ -28,33 +31,52 @@ function App() {
         </div>
         <div className="weather-data">
           <div style={{
-            display: "flex",
-            flexDirection: "column"
+            // display: "flex",
+            // alignItems: "center"
           }}>
             {showIcon && <img
+              style={{ width: "60px" }}
               src={weather ? `https:${weather.current.condition.icon}` : ""}
               alt={weather ? weather.current.condition.text : "weather icon"}
             />}
-            <span>{"Feels like"} {weather && weather.current.feelslike_c}&deg;C</span>
+            {weather && <p>{weather.location.name}, {weather.location.region}</p>}
           </div>
-          {weather && <p>{weather.location.name}, {weather.location.region}</p>}
-          {weather && <p>Temprature: {weather.current.temp_c}&deg;C</p>}
+          <div style={{
+            // display: "flex", flexDirection: "column",alignItems: "center"
+          }}>
+            {weather && <span style={{
+              // display: "flex", alignItems: "center"
+            }}><BsThermometerHalf size={"25px"} /> Feels like {weather.current.feelslike_c}&deg;C</span>}
+            {weather && <p>Temprature: {weather.current.temp_c}&deg;C</p>}
+          </div>
+
+          {weather?.current && (
+            <span
+              style={{
+                display: "inline-block",
+                transform: `rotate(${weather.current.wind_degree}deg)`,
+                transition: "transform 0.4s ease",
+                marginLeft: "5px"
+              }}
+            >
+              <FaLocationArrow style={{ fontSize: "1.5rem" }} />
+            </span>
+          )}
+          {weather && <p>{weather.current.wind_kph} km/h towards {weather.current.wind_dir}</p>}
+
+          <div style={{
+            // display: "flex",
+            // flexDirection: "column",
+            // justifyContent: "flex-start"
+          }}>
+            {weather && <p style={{
+              // display: "flex", alignItems: "center"
+            }}><WiBarometer size={"50px"} /> Pressure: {weather.current.pressure_mb} millibar</p>}
+            {weather && <p style={{
+              // display: "flex", alignItems: "center", gap: "10px"
+            }}><MdOutlineVisibility size={"30px"} /> Visibility: {weather.current.vis_km} kms</p>}
+          </div>
         </div>
-
-        {weather?.current && (
-          <span
-            style={{
-              display: "inline-block",
-              transform: `rotate(${weather.current.wind_degree}deg)`,
-              transition: "transform 0.4s ease",
-              marginLeft: "5px"
-            }}
-          >
-            <FaLocationArrow style={{ fontSize: "1.5rem" }} />
-          </span>
-        )}
-        {weather && <p>{weather.current.wind_kph} km/h towards {weather.current.wind_dir}</p>}
-
       </div>
     </>
   )
